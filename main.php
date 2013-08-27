8a5fc4d657c17486776d62fe7606d32bf0594eb0
Fabio Silva <fabio@yubo.be>
2013-08-27 17:22:45 +0100

    Full revision to use wp_widget class and make the plugin able to produce multiple widgets

--- main.php
+++ main.php
@@ -13,74 +13,97 @@ Any other notes about the plugin go here
 . 
 */  
 
-function widget_adSoc($args=array(), $params=array()) {
-  extract($args);
-  $widgettitle = get_option('adSoc_widget_title');
-  $widgetsource = get_option('adSoc_widget_adSoc');
-  $widgetdesc = get_option('adSoc_widget_description');
 
- echo $before_widget;
+/**
+ * Adds AdSoc_Widget widget.
+ */
+class AdSoc_Widget extends WP_Widget {
 
-  //Affix trial lacks div closure
-  //echo '<div class="alert sidebar-widget sidebar-nav" data-spy="affix" >';
-
-  echo $before_title.$widgettitle.$after_title;
-
-  echo stripslashes($widgetdesc);
-
-
-  echo $after_widget;
-}
-
-function adSoc_init(){
-
-wp_register_sidebar_widget(
-    'ad_soc_1',        // your unique widget id
-    'Ad Soc',          // widget name
-    'widget_adSoc',  // callback function
-    array(                  // options
-        'description' => 'Description of what your widget does'
-    )
-);
-
-wp_register_widget_control(
-	'ad_soc_1',		// id
-	'Ad Soc',		// name
-	'widget_adSoc_control'	// callback function
-);
-
-}
-
-function widget_adSoc_control($args=array(), $params=array()) {
-	//the form is submitted, save into database
-	if (isset($_POST['submitted'])) {
-		update_option('adSoc_widget_title', $_POST['widgettitle']);
-		update_option('adSoc_widget_adSource', $_POST['adsource']);
-		update_option('adSoc_widget_description', $_POST['description']);
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'ad_soc_widget', // Base ID
+			'Ad_Soc', // Name
+			array( 'description' => __( 'A Custom Ads Widget ofr SOC', 'text_domain' ), ) // Args
+		);
 	}
 
-	//load options
-	$widgettitle = get_option('adSoc_widget_title');
-	$adsource = get_option('adSoc_widget_adSource');
-	$description = get_option('adSoc_widget_description');
-	?>
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
 
-	Widget Title:<br />
-	<input type="text" style="width:100%" name="widgettitle" value="<?php echo stripslashes($widgettitle); ?>" />
-	<br /><br />
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+			echo $instance['adContent'];
+		echo $args['after_widget'];
+	}
 
-	Description about you:<br />
-	<textarea style="width:100%" rows="5" name="description"><?php echo stripslashes($description); ?></textarea>
-	<br /><br />
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
 
-	Cookie Value:<br />
-	<input type="text" style="width:100%" name="adsource" value="<?php echo stripslashes($adsource); ?>" />
-	<br /><br />
+                if ( isset( $instance[ 'adContent' ] ) ) {
+                        $adContent = $instance[ 'adContent' ];
+                }
+                else {
+                        $adContent = __( 'New Ad', 'text_domain' );
+                }
 
-	<input type="hidden" name="submitted" value="1" />
-	<?php
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<textarea style="width:100%" rows="5" id="<?php echo $this->get_field_id( 'adContent' ); ?>" name="<?php echo $this->get_field_name( 'adContent' );  ?>" type="text" value="<?php echo stripslashes( $adContent ); ?>"><?php echo stripslashes( $adContent ); ?></textarea>
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['adContent'] = ( ! empty( $new_instance['adContent'] ) ) ? stripslashes( $new_instance['adContent'] ) : '';
+
+		return $instance;
+	}
+
+}//End of CLASS
+
+// register AdSoc_Widget widget
+function register_adsoc_widget() {
+    register_widget( 'AdSoc_Widget' );
 }
+add_action( 'widgets_init', 'register_adsoc_widget' );
 
-add_action("plugins_loaded", "adSoc_init");
 
 ?>
